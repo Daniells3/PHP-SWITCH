601ece62c712f0af0711424e664b46be02309725
Daniells3 <daniells.xavier@etec.sp.gov.br>
2022-09-02 08:18:22 -0300

    TA CERTO AGORA

---
+++ Exercicio1.php
@@ -0,0 +1,54 @@
+<?php
+$meses = $_POST ['meses'];
+switch ($meses){
+
+
+case 1:
+    Echo "O mês atual é Janeiro";
+    break;
+case 2:
+    Echo "O mês atual é Fevereiro";
+    break;
+case 3:
+    Echo "O mês atual é Março";
+        break;
+
+ case 4:
+    Echo "O mês atual é Abril";
+     break;
+        
+ case 5:
+   Echo "O mês atual é Maio";
+      break;
+        
+case 6:
+     Echo "O mês atual é Junho";
+      break;
+                
+case 7:
+     Echo "O mês atual é Julho";
+     break;
+                    
+case 8:
+     Echo "O mês atual é Agosto";
+     break;
+                        
+case 9:
+      Echo "O mês atual é Setembro";
+   break;
+                            
+case 10:
+      Echo "O mês atual é Outubro";
+    break;
+                                
+case 11:
+      Echo "O mês atual é Novembro";
+    break;
+                                    
+case 12:
+      Echo "O mês atual é Dezembro";
+    break;
+                                        
+
+}
+?>
